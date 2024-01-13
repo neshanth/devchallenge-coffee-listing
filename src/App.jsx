4ae6745624375c4c63a3e5dafe170b6c26483448
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-13 11:07:05 +0530

    added bannner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import bgImg from "./assets/bg-cafe.jpg";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <section className="section-banner">
+        <img className="banner-image" src={bgImg} alt="banner" />
+      </section>
+    </main>
+  );
 };
 export default App;
