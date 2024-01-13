8179684c04356f4dd5436e4b5c68f5ca7cb9e4fe
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-13 14:40:45 +0530

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import CoffeeListing from "./components/CoffeeListing/CoffeeListing";
 const App = () => {
   const URL = `https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json`;
   const [coffeeListing, setCoffeeListing] = useState([]);
+  const [originalList, setOriginalList] = useState([]);
+  const [filter, setFilter] = useState("All");
   useEffect(() => {
     async function fetchCoffeeData() {
       try {
         const response = await fetch(URL);
         const data = await response.json();
         setCoffeeListing(data);
+        setOriginalList(data);
       } catch (error) {
         console.log(error);
       }
@@ -17,13 +20,23 @@ const App = () => {
     fetchCoffeeData();
   }, [URL]);
 
+  const handleCoffeFilter = (e, type) => {
+    e.preventDefault();
+    setFilter(type);
+    if (type === "Available") {
+      setCoffeeListing(coffeeListing.filter((coffee) => coffee.available));
+    } else {
+      setCoffeeListing(originalList);
+    }
+  };
+
   return (
     <main>
       <section className="section-banner">
         <img className="banner-image" src={bgImg} alt="banner" />
       </section>
       <section className="section-listing">
-        <CoffeeListing coffeeListing={coffeeListing} />
+        <CoffeeListing filter={filter} handleCoffeFilter={handleCoffeFilter} coffeeListing={coffeeListing} />
       </section>
     </main>
   );
--- src/components/CoffeeListing/CoffeeListing.jsx
+++ src/components/CoffeeListing/CoffeeListing.jsx
@@ -1,6 +1,6 @@
 import "./coffeeListing.css";
 import Card from "../Card/Card";
-const CoffeeListing = ({ coffeeListing }) => {
+const CoffeeListing = ({ coffeeListing, handleCoffeFilter, filter }) => {
   return (
     <div className="coffee-listing">
       <div className="coffee-lisiting-content">
@@ -9,8 +9,12 @@ const CoffeeListing = ({ coffeeListing }) => {
           Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
         </p>
         <div className="coffee-listing-filters">
-          <button>All Products</button>
-          <button>Available Now</button>
+          <button className={`${filter === "All" ? "coffee-listing-selected" : ""}`} onClick={(e) => handleCoffeFilter(e, "All")}>
+            All Products
+          </button>
+          <button className={`${filter === "Available" ? "coffee-listing-selected" : ""}`} onClick={(e) => handleCoffeFilter(e, "Available")}>
+            Available Now
+          </button>
         </div>
       </div>
       <div className="coffee-lists">
